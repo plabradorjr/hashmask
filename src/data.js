bdf16174dd6c32cada7124b4cefccc576e7ec4c1
Pancho Labrador <plabradorjr@gmail.com>
2021-02-06 03:49:44 -0600

    added wiz khalifa

--- src/data.js
+++ src/data.js
@@ -4,6 +4,8 @@ import hashmask from './assets/covers/hashmask1.png';
 import kream from './assets/songs/Kream.mp3';
 import roll from './assets/songs/on-a-roll.mp3';
 import dan from './assets/songs/dan-bilzerian.mp3';
+import dance from './assets/songs/dance.mp3';
+import assdrop from './assets/songs/ass-drop.mp3';
 
 function chillHop() {
   return [
@@ -21,7 +23,16 @@ function chillHop() {
       cover: hashmask,
       artist: "T-pain",
       audio: dan,
-      color: ["#1f2221", "#2ab3bf"],
+      color: ["#40b8d6", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Dance A$$",
+      cover: hashmask,
+      artist: "Big Sean",
+      audio: dance,
+      color: ["#d22bd8", "#2ab3bf"],
       id: uuidv4(),
       active: true,
     },
@@ -30,7 +41,16 @@ function chillHop() {
       cover: hashmask,
       artist: "Ashley O",
       audio: roll,
-      color: ["#1f2221", "#2ab3bf"],
+      color: ["#db23bd", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Ass Drop",
+      cover: hashmask,
+      artist: "Wiz Khalifa",
+      audio: assdrop,
+      color: ["#ec20db", "#2ab3bf"],
       id: uuidv4(),
       active: true,
     },
