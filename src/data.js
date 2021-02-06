9fea82bda4f893836a80b610994100dcb6e918f6
Pancho Labrador <plabradorjr@gmail.com>
2021-02-06 01:59:23 -0600

    added ashley o

--- src/data.js
+++ src/data.js
@@ -1,6 +1,8 @@
 import { v4 as uuidv4 } from "uuid";
 import thotiana from './assets/songs/thotiana.mp3';
 import hashmask from './assets/covers/hashmask1.png';
+import kream from './assets/songs/Kream.mp3';
+import roll from './assets/songs/on-a-roll.mp3'
 
 function chillHop() {
   return [
@@ -9,10 +11,29 @@ function chillHop() {
       cover: hashmask,
       artist: "Blue Face",
       audio: thotiana,
+      color: ["#ec20db", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "On A Roll",
+      cover: hashmask,
+      artist: "Ashley O",
+      audio: roll,
       color: ["#1f2221", "#2ab3bf"],
       id: uuidv4(),
       active: true,
     },
+    {
+      name: "Kream",
+      cover: hashmask,
+      artist: "Iggy Azalea",
+      audio: kream,
+      color: ["#b81c89", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    
     
     //ADD MORE HERE
   ];
